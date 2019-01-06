6d4f5041d13ef6265a94cbc115c832236768a93e
Will Strohl <will@upendoventures.com>
2019-01-06 14:03:14 -0800

    Initial updates to test generator-upendodnn

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,52 +1,46 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+'use strict'; 
+const Generator = require('yeoman-generator'); 
+const chalk = require('chalk'); 
+const yosay = require('yosay'); 
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay('Welcome to the ' + chalk.red('DNN') + ' project generator!'));
-    this.log(chalk.white('This scaffolds the project in your current directory.'));
+    this.log(yosay("Welcome to the Upendo Ventures " + chalk.red("DNN") + " project generator ! ")); 
+    this.log(chalk.white('This scaffolds the project in your current directory.')); 
 
-    const prompts = [
-      {
-        when: !this.options.projType,
-        type: 'list',
-        name: 'projType',
-        message: 'What type of project would you like to scaffold?',
-        choices: [
-          { name: 'MVC Module', value: 'mvc' },
-          { name: 'SPA Module', value: 'spa' },
-          {
-            name: chalk.gray('Persona Bar'),
-            value: 'personabar',
-            disabled: chalk.gray('Coming Soon')
-          },
-          {
-            name: chalk.gray('Theme'),
-            value: 'theme',
-            disabled: chalk.gray(
+    const prompts = [ {
+        when: ! this.options.projType, 
+        type:'list', 
+        name:'projType', 
+        message:'What type of project would you like to scaffold?', 
+        choices:[ {name:'Solution Structure', value:'solution'},  {name:'MVC Module', value:'mvc'},  {name:'SPA Module', value:'spa'},  {
+            name:chalk.gray('Persona Bar'), 
+            value:'personabar', 
+            disabled:chalk.gray('Coming Soon')
+          },  {
+            name:chalk.gray('Theme'), 
+            value:'theme', 
+            disabled:chalk.gray(
               'For the best starter DNN theme use nvQuickTheme (https://www.nvquicktheme.com)'
             )
           }
         ]
       }
-    ];
+    ]; 
 
-    return this.prompt(prompts).then(props => {
+    return this.prompt(prompts).then(props =>  {
       // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+      this.props = props; 
+    }); 
   }
 
   composing() {
-    const options = {
-      projType: this.props.value
-    };
+    const options =  {
+      projType:this.props.value
+    }; 
 
-    this.composeWith(require.resolve(`../${this.props.projType}`), options);
-    // This.composeWith(require.resolve('generator-fountain-gulp/generators/app'), options);
+    this.composeWith(require.resolve(`../$ {this.props.projType}`), options); 
   }
 
   writing() {}
@@ -54,4 +48,4 @@ module.exports = class extends Generator {
   install() {
     // This.installDependencies({ npm: true, bower: false, yarn: false });
   }
-};
+}; 
